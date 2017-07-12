16f2fcfa26723072136c616fddf184122ad0d184
OneChance <zhouh08@hotmail.com>
2017-07-12 17:21:03 +0800

    vector practice
    添加精度后,出现异常

---
+++ linear_algebra/vector_practice.py
@@ -0,0 +1,66 @@
+from math import *
+from decimal import Decimal, getcontext
+
+getcontext().prec = 30
+
+
+class Vector(object):
+    def __init__(self, coordinates):
+        try:
+            if not coordinates:
+                raise ValueError
+            self.coordinates = tuple([Decimal(x) for x in coordinates])
+            self.dimension = len(coordinates)
+        except ValueError:
+            raise ValueError('The coordinates must be nonempty')
+        except TypeError:
+            raise TypeError('The coordinates must be an iterable')
+
+    def __str__(self):
+        return 'Vector:{}'.format(self.coordinates)
+
+    def __eq__(self, v):
+        return self.coordinates == v.coordinates
+
+    def plus(self, v):
+        new_coordinates = [x + y for x, y in zip(self.coordinates, v.coordinates)]
+        return Vector(new_coordinates)
+
+    def minus(self, v):
+        new_coordinates = [x - y for x, y in zip(self.coordinates, v.coordinates)]
+        return Vector(new_coordinates)
+
+    def times_scalar(self, c):
+        new_coordinates = [Decimal(c) * x for x in self.coordinates]
+        return Vector(new_coordinates)
+
+    def magnitude(self):
+        coordinates_squared = [x ** 2 for x in self.coordinates]
+        return sqrt(sum(coordinates_squared))
+
+    def normalized(self):
+        try:
+            magnitude = self.magnitude()
+            return self.times_scalar(1.0 / magnitude)
+        except ZeroDivisionError:
+            raise Exception('Cannot normalize the zero vector')
+
+    def dot(self, v):
+        return sum([x * y for x, y in zip(self.coordinates, v.coordinates)])
+
+    def arc(self, v):
+        u1 = self.normalized()
+        u2 = v.normalized()
+        try:
+            return acos(u1.dot(u2))
+        except ZeroDivisionError:
+            raise Exception('cannot compute an angle with the zero vector')
+
+    def degree(self, v):
+        return self.arc(v) * 180 / pi
+
+
+my_vector = Vector([-7.579, -7.88])
+my_vector2 = Vector([22.737, 23.64])
+
+print my_vector.degree(my_vector2)
